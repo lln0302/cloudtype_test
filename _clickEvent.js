4a566984f9542261af39c3508f4ef56d253b7de2
woojun.Jung <96907223+lln0302@users.noreply.github.com>
2023-05-15 17:02:46 +0900

    Update _clickEvent.js

--- _clickEvent.js
+++ _clickEvent.js
@@ -1,5 +1,5 @@
 function fnClickObj(src){
-	console.log('mpva.metatour.net click:', src);
+	console.log('metatest.nztoverse.com click:', src);
 	
 
 	if( (screen.width <= 640) || (window.matchMedia && window.matchMedia('only screen and (max-width: 640px)').matches)){
@@ -8,7 +8,7 @@ function fnClickObj(src){
 
 		var str = "<table style='width:300px;' cellpadding='0' cellspacing='0' border='0'>";
 		str += "<tr><td><div align='center'><img src='https://mpva.metatour.net/img/guest_top_mobile.png' style='width:300px; height:50px;' id='btnClickClose-mobile'></div></td></tr>";
-		str += "<tr><td style='text-align:center; padding-top:0px; padding-bottom:0px;' align='center'><div align='center'><iframe src='https://mpva.metatour.net/_guestbook.html' style='width:300px; height:400px;' frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=yes vspace=0></iframe></div></td></tr>";
+		str += "<tr><td style='text-align:center; padding-top:0px; padding-bottom:0px;' align='center'><div align='center'><iframe src='https://metatest.nztoverse.com/_guestbook.html' style='width:300px; height:400px;' frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=yes vspace=0></iframe></div></td></tr>";
 		str += "</table>";
 		
 		document.getElementById("custom-popup-text-mobile").innerHTML = str;
@@ -19,7 +19,7 @@ function fnClickObj(src){
 
 		var str = "<table style='width:800px;' cellpadding='0' cellspacing='0' border='0'>";
 		str += "<tr><td><div align='center'><img src='https://mpva.metatour.net/img/guest_top_pc.png' style='width:800px; height:50px;' id='btnClickClose'></div></td></tr>";
-		str += "<tr><td style='text-align:center; padding-top:0px; padding-bottom:0px;' align='center'><div align='center'><iframe src='https://mpva.metatour.net/_guestbook.html' style='width:800px; height:600px;' frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=yes vspace=0></iframe></div></td></tr>";
+		str += "<tr><td style='text-align:center; padding-top:0px; padding-bottom:0px;' align='center'><div align='center'><iframe src='https://metatest.nztoverse.com/_guestbook.html' style='width:800px; height:600px;' frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=yes vspace=0></iframe></div></td></tr>";
 		str += "</table>";
 
 		document.getElementById("custom-popup-text").innerHTML = str;
