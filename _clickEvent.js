9819e8ffb9ddbd8b22a8e6f5bfef1d50728aa9da
woojun.Jung <96907223+lln0302@users.noreply.github.com>
2023-05-15 17:03:09 +0900

    Update _clickEvent.js

--- _clickEvent.js
+++ _clickEvent.js
@@ -8,7 +8,7 @@ function fnClickObj(src){
 
 		var str = "<table style='width:300px;' cellpadding='0' cellspacing='0' border='0'>";
 		str += "<tr><td><div align='center'><img src='https://mpva.metatour.net/img/guest_top_mobile.png' style='width:300px; height:50px;' id='btnClickClose-mobile'></div></td></tr>";
-		str += "<tr><td style='text-align:center; padding-top:0px; padding-bottom:0px;' align='center'><div align='center'><iframe src='https://metatest.nztoverse.com/_guestbook.html' style='width:300px; height:400px;' frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=yes vspace=0></iframe></div></td></tr>";
+		str += "<tr><td style='text-align:center; padding-top:0px; padding-bottom:0px;' align='center'><div align='center'><iframe src='https://metatest.nztoverse.com/_quiz.html' style='width:300px; height:400px;' frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=yes vspace=0></iframe></div></td></tr>";
 		str += "</table>";
 		
 		document.getElementById("custom-popup-text-mobile").innerHTML = str;
@@ -19,7 +19,7 @@ function fnClickObj(src){
 
 		var str = "<table style='width:800px;' cellpadding='0' cellspacing='0' border='0'>";
 		str += "<tr><td><div align='center'><img src='https://mpva.metatour.net/img/guest_top_pc.png' style='width:800px; height:50px;' id='btnClickClose'></div></td></tr>";
-		str += "<tr><td style='text-align:center; padding-top:0px; padding-bottom:0px;' align='center'><div align='center'><iframe src='https://metatest.nztoverse.com/_guestbook.html' style='width:800px; height:600px;' frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=yes vspace=0></iframe></div></td></tr>";
+		str += "<tr><td style='text-align:center; padding-top:0px; padding-bottom:0px;' align='center'><div align='center'><iframe src='https://metatest.nztoverse.com/_quiz.html' style='width:800px; height:600px;' frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=yes vspace=0></iframe></div></td></tr>";
 		str += "</table>";
 
 		document.getElementById("custom-popup-text").innerHTML = str;
