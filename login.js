ac9e742002034be8554e3112f9192879ae86a8f6
ncomzmetaverse <develop@ncomz.com>
2023-06-12 11:16:20 +0900

    잡수정

--- login.js
+++ login.js
@@ -1,19 +1,3 @@
-// Firebase 설정 정보를 넣어주세요.
-const firebaseConfig = {
-    apiKey: "AIzaSyD23BNt_CZ43ZasIVS3P_vMRFs6cW6TEr0",
-    authDomain: "fire-test-mozilla.firebaseapp.com",
-    databaseURL: "https://fire-test-mozilla-default-rtdb.firebaseio.com",
-    projectId: "fire-test-mozilla",
-    storageBucket: "fire-test-mozilla.appspot.com",
-    messagingSenderId: "169534096470",
-    appId: "1:169534096470:web:5bcd6ebfd76a311ee6e7f7",
-    measurementId: "G-ERQ3SDV3ZK"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  
   // 로그인 상태 확인
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
